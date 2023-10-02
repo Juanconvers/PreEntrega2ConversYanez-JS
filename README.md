cf260efa1489e60427c346a5357cd37293c90e8d
Juan Camilo Convers Yañez <juanconvers.legal@gmail.com>
2023-10-02 12:44:34 -0500

    Commit 2: Desarrollo de sistema de instanción de objetos que se agregan a un array por medio de un ciclo

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PreEntrega2ConversYanez-JS
 Repositorio de preentrega No. 2 - CODERHOUSE Javascript
+
+// READ FIRST:
+
+// ***** CALCULADORA PRA USOS JUDICIALES - DEFINICIÓN DE LA COMPETENCIA PARA PROCESOS DE PERTENENCIA EN COLOMBIA AÑO 2023 *****
+
+// Explicación del programa:
+// 1. El procesos judiciale de pertenencia juzga si alguien puede ser propietario de un inmueble sólo por el hecho de ocuparlo.
+// 2. La "competencia judicial" es definir a cual Juez al que le corresponde decidir el asunto, pues hay muchos tipos de jueces.
+// 3. La forma de definir esta competencia depende del valor del inmueble que se quiere adquirir. 
+//    En Colombia se le llama "cuantía" y dicho valor es el avalúo catastral del inmueble.
+// 4. Puede ser que no se quiera adquirir la totalidad del inmueble (100%), sino que se se quiera otro porcentaje.
+// 5. Esta calculadora determina el valor de la cuantía según el porcentaje que se pretenda adquirir.
+// 6. El valor puede tener tres categorías: 
+//                  1). Ser inferior a $46.400.000 COP --> Juzgado Municipal o de Pequeñas Causas
+//                  2). Estar entre $46.400.000 COP y $174.000.000 COP --> Juzgado Municipal
+//                  3). Ser mayor a $174.000.000 COP --> Juzgado del Circuito
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,19 @@
 
 
-// READ FIRST:
-
-// ***** CALCULADORA PRA USOS JUDICIALES - DEFINICIÓN DE LA COMPETENCIA PARA PROCESOS DE PERTENENCIA EN COLOMBIA AÑO 2023 *****
-
-// Explicación del programa:
-// 1. El procesos judiciale de pertenencia juzga si alguien puede ser propietario de un inmueble sólo por el hecho de ocuparlo.
-// 2. La "competencia judicial" es definir a cual Juez al que le corresponde decidir el asunto, pues hay muchos tipos de jueces.
-// 3. La forma de definir esta competencia depende del valor del inmueble que se quiere adquirir. 
-//    En Colombia se le llama "cuantía" y dicho valor es el avalúo catastral del inmueble.
-// 4. Puede ser que no se quiera adquirir la totalidad del inmueble (100%), sino que se se quiera otro porcentaje.
-// 5. Esta calculadora determina el valor de la cuantía según el porcentaje que se pretenda adquirir.
-// 6. El valor puede tener tres categorías: 
-//                  1). Ser inferior a $46.400.000 COP --> Juzgado Municipal o de Pequeñas Causas
-//                  2). Estar entre $46.400.000 COP y $174.000.000 COP --> Juzgado Municipal
-//                  3). Ser mayor a $174.000.000 COP --> Juzgado del Circuito
+// Nuevo contexto del proyecto:
+// Cálculo de copmpetencia de procesos de pertenencia:
+// 1. De varios inmuebles al tiempo porque es en el contexto de una firma de abogados grande.
+// 2. Sirve para sacar estadísticas de los procesos (urbano / rural) (cuantía)
 
 // Declaración de variables:
 
+let nombrePredio = "";
 let avaluo = 0;
 let porcentaje = 0;
-let cuantia = 0;
+let clasificacion = "";
+let caso = {};
+
+
 
 // Definición de la función que realiza el cálculo de la cuantía:
 
@@ -36,31 +29,137 @@ function calcularCuantia (avaluo, porcentaje) {
 alert("Bienvenido(a) - Este servicio le ayudará a saber a cuál tipo de juzgado le corresponde tramitar su proceso de pertenencia en Colombia.");
 
 let respuestaContinuar = prompt("¿Desea realizar un cálculo de competencia judicial? Responda Si / No");
+
+// Definición del Array que tendrá como elementos los casos (objetos) introducidos por el usuario en la consulta
+
+const listaCasos = [];
+
+
+//  Ciclo de introducción de datos y cálculo de cuantía 
+
 while (respuestaContinuar.toUpperCase() === "SI") {
   alert("Por favor responda las siguientes preguntas:");
  
-  avaluo = parseFloat(prompt("¿Cuál es avalúo catastral del inmueble que pretende o el de mayor extensión? Escriba el valor en pesos colombianos. Use números, sin puntos, comas u otros signos")); 
-  porcentaje = parseFloat (prompt("¿Cuál es el porcentaje que ocupa el predio que usted pretende en posesión? Escriba un valor del 0 al 100"));
-  cuantia = calcularCuantia (avaluo, porcentaje);
+// Instanciación del objeto que correponde a cada caso que se consulta
+
+    caso = {
+    nombrePredio: prompt("¿Cuál es nombre del predio a reclamar? O indique el nombre con que se conoce su caso"),
+    avaluo: parseFloat(prompt("¿Cuál es avalúo catastral del inmueble que pretende o el de mayor extensión? Escriba el valor en pesos colombianos. Use números, sin puntos, comas u otros signos")),
+    porcentaje: parseFloat (prompt("¿Cuál es el porcentaje que ocupa el predio que usted pretende en posesión? Escriba un valor del 0 al 100")),
+    tipo: prompt("¿El predio a reclamar es urbano o rural?")
+    }
+
+    listaCasos.push(caso);
+
+
+    console.log(listaCasos); 
+
 
 // Con cada resultado solicité impresión de los resultados numéricos en la consola para verificar el funcionamiento de la función.
+
+let cuantia = calcularCuantia (caso.avaluo, caso.porcentaje)
 
     if (cuantia !== NaN) {
       if (cuantia <= 46400000){
             alert("Su proceso se cataloga de Mínima Cuantía - Le corresponde radicar el proceso en un Juzgado Municipal o de Pequeñas Causas")
-            console.log("Avalúo catastral ingresado: $" + avaluo + ". Porcentaje sobre el avalúo:" + porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
+            console.log("Avalúo catastral ingresado: $" + caso.avaluo + ". Porcentaje sobre el avalúo:" + caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
           } else if (46400000 < cuantia && cuantia <= 174000000){
             alert("Su proceso se cataloga de Menor Cuantía - Le corresponde radicar el proceso en un Juzgado Municipal");
-            console.log("Avalúo catastral ingresado: $" + avaluo + ". Porcentaje sobre el avalúo:" + porcentaje + "% . Cuantía de la pretensión: $" + cuantia ); 
+            console.log("Avalúo catastral ingresado: $" + caso.avaluo + ". Porcentaje sobre el avalúo:" + caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia ); 
           } else if (174000000 < cuantia){
-            alert("Su proceso se cataloga de Mayor Cuantía - Le corresponde radicar el proceso en un Juazgado del Circuito");
-            console.log("Avalúo catastral ingresado: $" + avaluo + ". Porcentaje sobre el avalúo:" + porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
+            alert("Su proceso se cataloga de Mayor Cuantía - Le corresponde radicar el proceso en un Juzgado del Circuito");
+            console.log("Avalúo catastral ingresado: $" + caso.avaluo + ". Porcentaje sobre el avalúo:" + caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
           } else {
             alert("No se puede procesar la solicitud"); 
           }
     }
+
+    //     //Declaraciòn de array vacío y variable para determinar cantidad
+    // const listaCasos = [];
+    //    //Empleo de do...while para cargar nombres en el array por prompt()
+   
+    // listaCasos.push(Caso);
+    // console.log(listaCasos);
+    
+
     // Opción para realizar un nuevo cálculo de jurisdicción o terminar.
     respuestaContinuar = prompt("¿Desea realizar otro cálculo de competencia judicial? Responda Si / No"); 
 }
-
 alert("Gracias por elegirnos");
+
+
+
+// // alert("Avalúo catastral ingresado: $" + Caso.avaluo + ". Porcentaje sobre el avalúo:" + Caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia + "Tipo de inmueble:" + Caso.tipo);
+
+
+// ******************************   FUNCIONA **********************
+ 
+// // Declaración de variables:
+
+// let nombrePredio = "";
+// let avaluo = 0;
+// let porcentaje = 0;
+// let clasificacion = "";
+
+
+
+// // Definición de la función que realiza el cálculo de la cuantía:
+
+// function calcularCuantia (avaluo, porcentaje) {
+//   if (avaluo !== NaN && porcentaje !== NaN) {
+//     return (avaluo * porcentaje) / 100;
+//   }
+//   return NaN;
+// }
+
+// // Introducción
+
+// alert("Bienvenido(a) - Este servicio le ayudará a saber a cuál tipo de juzgado le corresponde tramitar su proceso de pertenencia en Colombia.");
+
+// let respuestaContinuar = prompt("¿Desea realizar un cálculo de competencia judicial? Responda Si / No");
+// while (respuestaContinuar.toUpperCase() === "SI") {
+//   alert("Por favor responda las siguientes preguntas:");
+ 
+// // Instanciación del objeto que correponde a cada caso que se consulta
+
+//   let Caso = {
+//     nombrePredio: prompt("¿Cuál es nombre del predio a reclamar? O indique el nombre con que se conoce su caso"),
+//     avaluo: parseFloat(prompt("¿Cuál es avalúo catastral del inmueble que pretende o el de mayor extensión? Escriba el valor en pesos colombianos. Use números, sin puntos, comas u otros signos")),
+//     porcentaje: parseFloat (prompt("¿Cuál es el porcentaje que ocupa el predio que usted pretende en posesión? Escriba un valor del 0 al 100")),
+//     tipo: prompt("¿El predio a reclamar es urbano o rural?")
+//     }
+// console.log(Caso); 
+
+
+
+// // Con cada resultado solicité impresión de los resultados numéricos en la consola para verificar el funcionamiento de la función.
+
+// let cuantia = calcularCuantia (Caso.avaluo, Caso.porcentaje)
+
+//     if (cuantia !== NaN) {
+//       if (cuantia <= 46400000){
+//             alert("Su proceso se cataloga de Mínima Cuantía - Le corresponde radicar el proceso en un Juzgado Municipal o de Pequeñas Causas")
+//             console.log("Avalúo catastral ingresado: $" + Caso.avaluo + ". Porcentaje sobre el avalúo:" + Caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
+//           } else if (46400000 < cuantia && cuantia <= 174000000){
+//             alert("Su proceso se cataloga de Menor Cuantía - Le corresponde radicar el proceso en un Juzgado Municipal");
+//             console.log("Avalúo catastral ingresado: $" + Caso.avaluo + ". Porcentaje sobre el avalúo:" + Caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia ); 
+//           } else if (174000000 < cuantia){
+//             alert("Su proceso se cataloga de Mayor Cuantía - Le corresponde radicar el proceso en un Juzgado del Circuito");
+//             console.log("Avalúo catastral ingresado: $" + Caso.avaluo + ". Porcentaje sobre el avalúo:" + Caso.porcentaje + "% . Cuantía de la pretensión: $" + cuantia );
+//           } else {
+//             alert("No se puede procesar la solicitud"); 
+//           }
+//     }
+
+//         //Declaraciòn de array vacío y variable para determinar cantidad
+//     const listaCasos = [];
+//        //Empleo de do...while para cargar nombres en el array por prompt()
+   
+//     listaCasos.push(Caso);
+//     console.log(listaCasos);
+    
+
+//     // Opción para realizar un nuevo cálculo de jurisdicción o terminar.
+//     respuestaContinuar = prompt("¿Desea realizar otro cálculo de competencia judicial? Responda Si / No"); 
+// }
+// alert("Gracias por elegirnos");
